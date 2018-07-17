9a453f1ab2fa3d1cd5120648c0fec6214972a48a
Xie Zhaoheng <411317572@qq.com>
2018-07-17 10:43:36 +0800

    修改LoginForm表单

--- app/main/forms/baseforms.py
+++ app/main/forms/baseforms.py
@@ -1,11 +1,18 @@
+# -*- coding:utf-8 -*-
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, Regexp
-from wtforms import ValidationError
+from wtforms import StringField, TextField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
 
 
 class LoginForm(FlaskForm):
-    userName = StringField('UserName', validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    remember_me = BooleanField("RememberMe")
-    submit = SubmitField('Log In')
+    account = StringField(label="账号", validators=[DataRequired("请输入账号!")],
+                          render_kw={"class": "layui-input", "placeholder": "请输入用户名！", "required": "required",
+                                     "lay-verify": "required"})
+    pwd = PasswordField(label="密码", validators=[DataRequired("请输入密码!")],
+                        render_kw={"class": "layui-input", "placeholder": "请输入密码！", "required": "required",
+                                   "lay-verify": "required"})
+    submit = SubmitField(label="登录", render_kw={"class": "layui-btn btn-login"})
+
+    def validate_account(self, field):  # 自定义验证器, 规则是"validate_字段名"
+        pass
+
