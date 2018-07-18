190bbfbd36c62be08bf0472a6f0614c6d8271d11
Xie Zhaoheng <411317572@qq.com>
2018-07-18 09:05:57 +0800

    同一个局域网内,不同的wifi无法访问,调试中...

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 from flask_login import LoginManager
 
 from config.appConfig import config
@@ -12,7 +12,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__, template_folder='demo/template', static_folder='demo/static')
-    # CORS(app, supports_credentials=True)
+    CORS(app, supports_credentials=True)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
